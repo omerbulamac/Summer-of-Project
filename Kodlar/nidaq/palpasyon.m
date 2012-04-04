0ebcc27bf21b2e6638c9698daaf47cf97ab5fd74
unknown <cehars@ceng.(none)>
2012-04-05 02:24:24 +0300

    palpasyon

--- Kodlar/nidaq/palpasyon.m
+++ Kodlar/nidaq/palpasyon.m
@@ -1,8 +1,9 @@
 while true
-    dio = digitalio('nidaq','Dev1');
-    addline(dio,0,'in');    
-    portval = getvalue(dio);
     
+    dio = digitalio('nidaq','Dev1');
+    addline(dio,0,'in');
+    portval = getvalue(dio);
+
     if portval == 0
         fprintf('basti\n')
     else
