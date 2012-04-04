a0330c9120001db96a42d4538501e26852f647f7
unknown <cehars@ceng.(none)>
2012-04-05 02:20:10 +0300

    palpasyon

---
+++ Kodlar/nidaq/palpasyon.m
@@ -0,0 +1,12 @@
+while true
+    dio = digitalio('nidaq','Dev1');
+    addline(dio,0,'in');    
+    portval = getvalue(dio);
+    
+    if portval == 0
+        fprintf('basti\n')
+    else
+        fprintf('basili degil\n')
+    end
+
+end
