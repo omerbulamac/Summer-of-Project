afb4d1c8932f8a7454f174e4b11c9792045db355
unknown <cehars@ceng.(none)>
2012-06-23 04:37:32 +0300

    ecg sinyali

---
+++ Kodlar/Matlab/winsound/ecg sinyali/ekran_ciktisi.m
@@ -0,0 +1,24 @@
+clc;clear all;
+
+AO = analogoutput('winsound', 0);
+
+chan = addchannel(AO, 1);
+
+duration = 5;
+set(AO,'SampleRate',10000)
+set(AO,'TriggerType','Manual')
+ActualRate = get(AO,'SampleRate');
+len = ActualRate*duration;
+%data = 4*sin(linspace(0,2*pi*500,len))';
+data = ecgsyn;
+putdata(AO,data);
+
+get (AO, 'SamplesOutput')
+
+plot(data)
+start(AO)
+trigger(AO)
+wait(AO,5)
+
+delete(AO)
+clear AO
