57d47789f569580973fec6f2af7abcc6a9c934ee
unknown <cehars@ceng.(none)>
2012-06-23 04:43:24 +0300

    duzenleme

--- Proje/ecg sinyali/sinus sinyali/ekran_ciktisi.m
+++ Proje/ecg sinyali/sinus sinyali/ekran_ciktisi.m
@@ -1,21 +1,20 @@
 clc;clear all;
 
-AO = analogoutput('winsound', 0);
-
-chan = addchannel(AO, 1);
+AO = analogoutput('nidaq','Dev1');
+chan = addchannel(AO,0);
 
 duration = 5;
 set(AO,'SampleRate',10000)
 set(AO,'TriggerType','Manual')
 ActualRate = get(AO,'SampleRate');
 len = ActualRate*duration;
-%data = 4*sin(linspace(0,2*pi*500,len))';
-data = ecgsyn;
+data = 4*sin(linspace(0,2*pi*500,len))';
 putdata(AO,data);
 
 get (AO, 'SamplesOutput')
 
 plot(data)
+
 start(AO)
 trigger(AO)
 wait(AO,5)
